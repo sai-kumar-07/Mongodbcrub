dc41a01943b5cd4da2457cfd50d496575cd57d56
sai-kumar-07 <158156243+sai-kumar-07@users.noreply.github.com>
2024-06-11 14:21:26 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,2 +1,2 @@
-module.exports = `mongodb://localhost:27017`
-//module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/"
+// module.exports = `mongodb://localhost:27017`
+module.exports = mongodb+srv://admin>@cluster0.7kffpfy.mongodb.net/
